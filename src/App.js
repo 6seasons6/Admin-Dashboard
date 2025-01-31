e1f5d1351d3d663c428152809f8d413312e1c735
swapna pasham <swapnapasham553@gmail.com>
2025-01-31 15:59:49 +0530

    sidepanel changes

--- src/App.js
+++ src/App.js
@@ -1,31 +1,28 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Sidebar from './components/Sidebar';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Dashboard from './pages/Dashboard';
-import Users from './pages/Users';
-import Graphs from './pages/Reports';
-import './App.css'; // Make sure the path is correct
-import { AuthProvider } from './contexts/AuthContext';
-import UserList from './components/UserManagement/UserList';
-import ProductList from './components/ProductManagement/ProductList';
-import SalesReport from './components/Analytics/SalesReport';
-import UserActivity from './components/Analytics/UserActivity';
-import Login from './components/Auth/Login';
-import Register from './components/Auth/Register';
-import PersonalisedDashboard from './pages/PersonalisedDashboard';
-import ProductTable from './components/ProductManagement/ProductTable';
-import ProductForm from './components/ProductManagement/ProductForm';
-import ForgotPassword from './components/Auth/ForgotPassword';
-//import ResetPassword from './components/Auth/ResetPassword';
-import UserForm from './components/UserManagement/UserForm';
-import UserTable from './components/UserManagement/UserTable';
-import { createTheme } from '@mui/material/styles';
-import SettignPage from './pages/Settingpage';
-import SupportPage from './pages/Supportpage';
-
-
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import Sidebar from "./components/Sidebar";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Dashboard from "./pages/Dashboard";
+import Graphs from "./pages/Reports";
+import "./App.css";
+import { AuthProvider } from "./contexts/AuthContext";
+import UserList from "./components/UserManagement/UserList";
+import ProductList from "./components/ProductManagement/ProductList";
+import SalesReport from "./components/Analytics/SalesReport";
+import UserActivity from "./components/Analytics/UserActivity";
+import Login from "./components/Auth/Login";
+import Register from "./components/Auth/Register";
+import PersonalisedDashboard from "./pages/PersonalisedDashboard";
+import ProductTable from "./components/ProductManagement/ProductTable";
+import ProductForm from "./components/ProductManagement/ProductForm";
+import ForgotPassword from "./components/Auth/ForgotPassword";
+import UserForm from "./components/UserManagement/UserForm";
+import UserTable from "./components/UserManagement/UserTable";
+import SettingPage from "./pages/Settingpage";
+import SupportPage from "./pages/Supportpage";
+import { createTheme } from "@mui/material";
+import SalesAnalytics from "./components/Analytics/SalesAnalytics";
 const theme = createTheme({
   palette: {
     primary: {
@@ -41,76 +38,63 @@ const theme = createTheme({
   spacing: 8, // Default spacing
 });
 
-
-
-
-const App = () => {
+ 
+const Layout = ({ children }) => {
+  const location = useLocation();
+ 
+  // Hide Sidebar only when there is no path (i.e., homepage `/`)
+  const showSidebar = location.pathname !== "/";
+ 
   return (
-    <AuthProvider>
-      <Router>
-        <div className="app">
-          <Sidebar />
-          <div className="main-content">
-            <Navbar />
-            <div className="content">
-              <Routes>
-                {/* Main Routes */}
-                <Route path="/" element={<Dashboard />} />
-
-                <Route path="/users" element={<UserList />} />
-                <Route path="/products" element={<ProductList />} />
-
-                <Route path="/dashboard" element={<PersonalisedDashboard />} />
-                
-                <Route path="/users" element={<UserList />} />
-
-                <Route path="/ProductList" element={<ProductList />} />
-                <Route path="/ProductForm" element={<ProductForm/>} />
-                <Route path="/ProductTable" element={<ProductTable/>} />
-
-
-                <Route path="/users/new" element={<UserForm />} /> {/* For adding a new user */}
-                <Route path="/users/edit/:userId" element={<UserForm />} /> 
-                <Route path="/user-table" element={<UserTable />} />
-                
-                <Route path="/products" element={<ProductList />} />
-                <Route path="/ProductForm" element={<ProductForm />} />
-
-
-                {/* Analytics Routes */}
-                <Route path="/analytics/sales" element={<SalesReport />} />
-                <Route path="/analytics/activity" element={<UserActivity />} />
-
-
-                {/* Authentication Routes */}
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-
-
-                <Route path="./pages/Dashboard.js" element={<Dashboard/>}/>
-
-                <Route path="/forgot-password" element={<ForgotPassword />} />
-
-                
-
-
-
-                {/* Legacy Routes (if needed) */}
-                <Route path="/users-old" element={<Users />} />
-                <Route path="/reports" element={<Graphs />} />
-                <Route path="/settingpage" element={<SettignPage />} />
-                <Route path="/supportpage" element={<SupportPage />} />
-
-              </Routes>
-            </div>
-
-            <Footer />
-          </div>
-        </div>
-      </Router>
-    </AuthProvider>
+<div className="app" style={{ display: "flex" }}>
+      {showSidebar && <Sidebar />} {/* Sidebar appears on all pages except `/` */}
+<div className="main-content" style={{ flex: 1 }}>
+<Navbar />
+<div className="content">{children}</div>
+<Footer />
+</div>
+</div>
   );
 };
+ 
+const App = () => {
+  return (
+<AuthProvider>
+<Router>
+<Layout>
+<Routes>
+            {/* Main Routes */}
+<Route path="/" element={<Dashboard />} />
+<Route path="/dashboard" element={<PersonalisedDashboard />} />
+<Route path="/users" element={<UserList />} />
+<Route path="/products" element={<ProductList />} />
+<Route path="/ProductList" element={<ProductList />} />
+<Route path="/ProductForm" element={<ProductForm />} />
+<Route path="/ProductTable" element={<ProductTable />} />
+<Route path="/users/new" element={<UserForm />} />
+<Route path="/users/edit/:userId" element={<UserForm />} />
+<Route path="/user-table" element={<UserTable />} />
+ 
+            {/* Analytics Routes */}
+<Route path="/analytics/sales" element={<SalesReport />} />
+<Route path="/analytics/activity" element={<UserActivity />} />
+<Route path="/analytics" element={<SalesAnalytics />} />
+ 
+            {/* Authentication Routes */}
+<Route path="/login" element={<Login />} />
+<Route path="/register" element={<Register />} />
+<Route path="/forgot-password" element={<ForgotPassword />} />
+ 
+            {/* Other Pages */}
+<Route path="/reports" element={<Graphs />} />
+<Route path="/settingpage" element={<SettingPage />} />
+<Route path="/supportpage" element={<SupportPage />} />
 
+</Routes>
+</Layout>
+</Router>
+</AuthProvider>
+  );
+};
+ 
 export default App;
-
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,26 +2,27 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 
-import { FaHome, FaUsers, FaChartLine, FaProductHunt, FaUserCircle } from 'react-icons/fa';
+import { FaHome,FaChartLine, FaProductHunt, FaCalendar } from 'react-icons/fa';
 
 import { SiGoogleanalytics } from 'react-icons/si';
 import { IoSettings } from 'react-icons/io5';
+import image from '../images/image.png'
+
 
 
 
 const Sidebar = () => (
   <div className="sidebar">
     <div className="logo1 text-center">
-    <FaUserCircle />
+    <img src={image} alt='logo'></img>
     </div>
     <nav>
     <ul className='listitems'>
-      <li ><Link to="/"><FaHome className='icons'/> <span className='d-none d-sm-inline'> Dashboard</span> </Link></li>
+      <li><Link to="/"><FaHome className='icons'/> <span className='d-none d-sm-inline'> Dashboard</span> </Link></li>
       <li><Link to="/"><FaProductHunt  className='icons'/> <span className='d-none d-sm-inline'> Product Management</span> </Link></li>
-      <li><Link to="/"><SiGoogleanalytics className='icons'/> <span className='d-none d-sm-inline'> Analytics</span> </Link></li>
-
-      <li><Link to="/users"><FaUsers className='icons'/> <span className='d-none d-sm-inline'> Users</span> </Link></li>
-      <li><Link to="/reports"><FaChartLine className='icons'/> <span className='d-none d-sm-inline'> Reports</span> </Link></li>
+      <li><Link to="/reports"><SiGoogleanalytics className='icons'/> <span className='d-none d-sm-inline'> Analytics</span> </Link></li>
+      <li><Link to="/"><FaCalendar  className='icons'/> <span className='d-none d-sm-inline'> Calendar</span> </Link></li>
+      <li><Link to="/analytics"><FaChartLine className='icons'/> <span className='d-none d-sm-inline'> Reports</span> </Link></li>
       <li><Link to="/"><IoSettings className='icons'/> <span className='d-none d-sm-inline'> Settings</span> </Link></li>
 
 
